082cf1a346f9169ba2d339a14b54103a0bf07fab
Kiev Andres <kievmendoza30@gmail.com>
2025-09-04 12:25:14 -0400

    Setup first steps building portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'main',
+    loadComponent: () => import('./portfolio/pages/main-page/main-page')
+  },
+  {
+    path: '**',
+    redirectTo: 'main'
+  }
+];
---
+++ src/app/portfolio/pages/main-page/main-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-page',
+  imports: [],
+  templateUrl: './main-page.html',
+  styleUrl: './main-page.scss',
+})
+export default class MainPage { }
