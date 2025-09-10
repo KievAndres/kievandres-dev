87a597cea2e7b80d8c0563d507894418dd725ed2
Kiev Andres <kievmendoza30@gmail.com>
2025-09-10 15:20:09 -0400

    add stellar cube

--- src/app/portfolio/pages/main-page/main-page.ts
+++ src/app/portfolio/pages/main-page/main-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Particles } from "@shared/components/particles/particles";
 import { Sparkles } from "@shared/components/sparkles/sparkles";
+import { StellarCube } from "@/app/shared/components/stellar-cube/stellar-cube";
 
 @Component({
   selector: 'main-page',
-  imports: [Particles, Sparkles],
+  imports: [Particles, Sparkles, StellarCube],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
---
+++ src/app/shared/components/stellar-cube/stellar-cube.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stellar-cube',
+  imports: [],
+  templateUrl: './stellar-cube.html',
+  styleUrl: './stellar-cube.scss',
+})
+export class StellarCube { }
